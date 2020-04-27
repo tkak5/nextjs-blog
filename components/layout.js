99506df4bf3e753b1ef950ec87628c5bd484e5ed
三輪貴陽 <tkak@sanrinkyounoMBP.AirPort>
2020-04-28 07:00:36 +0900

    test

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'taka m'
+const name = 'takaaki miwa'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
